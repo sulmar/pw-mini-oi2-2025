9c44476efe7b9f8c8ce94e7c1068b66da6165a3d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-02 17:28:01 +0100

    Dodanie projektu TestApp

---
+++ src/TestApp/Fundamentals/Logger.cs
@@ -0,0 +1,18 @@
+namespace TestApp
+{
+    public class Logger
+    {
+        public string LastMessage { get; set; }
+
+        public void Log(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentNullException();
+
+            LastMessage = message;
+
+            // Write the log to a storage
+            // ...
+        }
+    }
+}
