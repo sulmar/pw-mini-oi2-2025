1ab9a6c1f6e1577c8869a0341a747b9774e4decc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-10 09:54:59 +0100

    Refactor: Zastosowanie słownika z kuponami rabatowymi

--- src/TestApp/TDD/DiscountPriceCalculator.cs
+++ src/TestApp/TDD/DiscountPriceCalculator.cs
@@ -2,19 +2,24 @@ namespace TestApp.TDD;
 
 public class DiscountPriceCalculator
 {
+    private IDictionary<string, decimal> _couponCodes;
+
+    public DiscountPriceCalculator()
+    {
+        _couponCodes = new Dictionary<string, decimal>();
+        _couponCodes.Add("SAVE10NOW", 0.1M);        // 10%
+        _couponCodes.Add("DISCOUNT20OFF", 0.2M);    // 20%
+    }
+    
     public decimal CalculateTotalPrice(decimal originalPrice, string couponCode)
     {
         if (originalPrice < 0)
             throw new ArgumentException("Negatives not allowed");
         
-        if (couponCode == "SAVE10NOW")
-            return originalPrice - originalPrice * 0.1M; // 10%
+        if (string.IsNullOrEmpty(couponCode))
+            return originalPrice;
+              
+        return originalPrice - originalPrice * _couponCodes[couponCode]; 
         
-        if (couponCode == "DISCOUNT20OFF")
-            return originalPrice - originalPrice * 0.2M; // 20%
-        
-        return originalPrice;
-
-        throw new NotImplementedException();
     }
 }
